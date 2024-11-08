04b91c9308e041bb8c356c6ae25c0086e796181b
WarwickRen <sorooshesmailiofficial@gmail.com>
2024-11-08 11:06:48 +0330

    stage 0 - initial conditions and user inputs

---
+++ isValidGraphSet.m
@@ -0,0 +1,37 @@
+function [check,E,V_star,isolates] =isValidGraphSet(V)
+    check=false; 
+    E=''; 
+    V_star=[];
+    isolates=0; 
+    
+    try % V only has positive integers
+        mustBeInteger(V), mustBeNonnegative(V)
+    catch
+        E='must contain only positive integers!';
+        return
+    end
+    try % V is not empty
+        mustBeNonNan(V), mustBeNonempty(V)
+    catch
+        E='must not be empty!';
+        return
+    end
+    try % V is finite
+         mustBeFinite(V)
+    catch
+        E='must be finite!';
+        return
+    end
+    if rem(sum(V),2) % q is even
+        E='adds up to an odd amount!';
+        return
+    end
+    isolates=sum(V==0);
+    V_star=sort(V(V~=0));
+    if sum(V_star>=length(V_star)) % deg(v_i)<n
+        E='may have a degree larger than the possible amount!';
+        return
+    end
+    check=true;
+end
+
--- main.m
+++ main.m
@@ -1,3 +1,12 @@
 clear
 clc
-V='V?'
+%% Initial Stage
+while 1
+    V=input('V?');
+    [check,E,V_star,isolates]=isValidGraphSet(V);
+    if check 
+        break
+    end
+    fprintf('Error! The array of vertice degrees %s\n',E)
+end
+%% Stage 1
